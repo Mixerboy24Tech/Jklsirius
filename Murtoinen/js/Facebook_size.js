defe7c501f89385cc7219cd031d2615da607ac15
Atte Oksanen <37768995+Mixerboy24@users.noreply.github.com>
2019-03-03 10:45:17 +0200

    Delete Facebook_size.js

--- Murtoinen/js/Facebook_size.js
+++
@@ -1,8 +0,0 @@
-$(window).resize(function () {
-    //Do the reload of plugin
-    var new_width = $("#facebook").parent().width();
-    $("#facebook").css("width", new_width);
-    var url = $('#facebook').attr('src').split("&width=");
-    url = url[0] + '&width=' + new_width;
-    $('#facebook').attr('src', url);
-});
